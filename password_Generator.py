50f2bda9c884f23bb37372d32f5cf96bba2a70b3
Paul <Voicupaul551@yahoo.com>
2022-01-22 16:03:46 +0200

    Creating the lists for all the possible characters that a password can have

--- password_Generator.py
+++ password_Generator.py
@@ -1,2 +1,7 @@
 #TODO: Create a program that generates random passwords based on the user's chocie and save them an encrypted place in the system
-f1
+from random import choices, shuffle
+from os import path
+small_letters=[chr(x) for x  in range(97,123)]
+captalized_letters=[chr(x) for x  in range(65,91)]
+numbers=[chr(x) for x  in range(48,58)]
+special_chrs=[chr(x) for x  in range(32,48)]+([chr(x) for x  in range(58,65)])+([chr(x) for x  in range(91,97)])+([chr(x) for x  in range(123,127)])
