c07e9f3164cd65124cdb7ff9dd95316c0ebece13
Paul <Voicupaul551@yahoo.com>
2022-01-29 12:07:25 +0200

    Complex user input handling

--- password_Generator.py
+++ password_Generator.py
@@ -12,14 +12,15 @@ def validate_password(password):
     return password
 
 def validate_user_input(user_input,input_type):
-    valid_answer = ["Yes", "No", "yes", "no","y","n","Y","N"]
-    any_answer=[str(x) for x in valid_answer]
+    valid_yes_answer = ["Yes", "yes", "y","Y"]
+    valid_no_answer = [ "No","no","n","N"]
+    any_answer=[str(x) for x in valid_yes_answer+valid_no_answer]
     possible_choices=",".join(x for x in any_answer)
     x = 0
     while x == 0:
         try:
             user_input=input(f"Do you want {input_type} in your password\n")
-            if user_input in valid_answer:
+            if user_input in valid_yes_answer+valid_no_answer:
                 x = -1
             else:
                 raise ValueError(f"That is not a valid answer. Choose between {possible_choices}. Please try again.")
@@ -42,18 +43,29 @@ def ask_for_input():
 small_letters=[chr(x) for x  in range(97,123)]
 captalized_letters=[chr(x) for x  in range(65,91)]
 digits=[chr(x) for x  in range(48,58)]
-special_chrs=[chr(x) for x  in range(32,48)]+([chr(x) for x  in range(58,65)])+([chr(x) for x  in range(91,97)])+([chr(x) for x  in range(123,127)])
+special_chrs=[chr(x) for x in range(32,48)]+([chr(x) for x in range(58,65)])+([chr(x) for x in range(91,97)])+([chr(x) for x in range(123,127)])
 inputs=ask_for_input()
 print(type(inputs))
-print(inputs)
+print(inputs[0])
 digits_in_password=0
 small_letters_in_password=0
-big_letters_in_password=0
+capitalized_letters_in_password=0
 special_chrs_in_password=0
-# all_in_password=digits_in_password+small_letters_in_password+big_letters_in_password+special_chrs_in_password
-# while not all_in_password==ask_for_input:
-#     x=randint(1,ask_for_input(1))
-#     y=randint(1,ask_for_input(1))
-#     z=randint(1,ask_for_input(1))
-#     all_in_password=digits_in_password+small_letters_in_password+big_letters_in_password+special_chrs_in_password
-# print(all_in_password)
+password=""
+characters_in_password=""
+yes=["Yes", "yes", "y","Y"]
+no=["No","no","n","N"]
+list_of_answers=list(inputs[0:5])
+print(list_of_answers)
+if list_of_answers[2:5] not in no:
+    characters_in_password=capitalized_letters_in_password+small_letters_in_password+digits_in_password+special_chrs_in_password
+    while not characters_in_password==inputs[1]:
+        capitalized_letters_in_password=randint(1,inputs[1])
+        small_letters_in_password=randint(1,inputs[1])
+        digits_in_password=randint(1,inputs[1])
+        special_chrs_in_password=randint(1,inputs[1])
+        characters_in_password=capitalized_letters_in_password+small_letters_in_password+digits_in_password+special_chrs_in_password
+print(characters_in_password)
+
+
+
