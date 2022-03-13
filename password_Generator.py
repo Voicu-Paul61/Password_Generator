8c5888d7f18841cb21b89cb9da4b7c7d8cefa3d6
Paul <Voicupaul551@yahoo.com>
2022-03-13 17:46:23 +0200

    Generated passwords save to file chosen by user

--- password_Generator.py
+++ password_Generator.py
@@ -3,7 +3,6 @@ from random import choices, shuffle, randint
 from os import path
 
 def create_password():
-    inputs=ask_for_input()
     password_string=str()
     generated_password=list()
     captalized_letters_in_password=list()
@@ -14,27 +13,26 @@ def create_password():
     number_of_digits_in_password = 0
     number_of_small_letters_in_password = 0
     number_of_capitalized_letters_in_password = 0
-
     number_of_special_chrs_in_password = 0
-    while not number_of_characters_in_password == inputs[1]:
-        if inputs[2] in yes:
-            number_of_capitalized_letters_in_password = randint(1, inputs[1])
+    while not number_of_characters_in_password == password_len:
+        if captalized_letters_exists in yes:
+            number_of_capitalized_letters_in_password = randint(1, password_len)
             number_of_characters_in_password = number_of_characters_in_password + number_of_capitalized_letters_in_password
             captalized_letters_in_password=choices(captalized_letters,k=number_of_capitalized_letters_in_password)
             # print(number_of_capitalized_letters_in_password)
         else:
             number_of_capitalized_letters_in_password = 0
             # print(number_of_capitalized_letters_in_password)
-        if inputs[3] in yes:
-            number_of_small_letters_in_password = randint(1, inputs[1])
+        if small_letters_exists in yes:
+            number_of_small_letters_in_password = randint(1, password_len)
             number_of_characters_in_password = number_of_characters_in_password + number_of_small_letters_in_password
             small_letters_in_password=choices(small_letters,k=number_of_small_letters_in_password)
             # print(number_of_small_letters_in_password)
         else:
             number_of_small_letters_in_password = 0
             # print(number_of_small_letters_in_password)
-        if inputs[4] in yes:
-            number_of_digits_in_password = randint(1, inputs[1])
+        if digits_exists in yes:
+            number_of_digits_in_password = randint(1, password_len)
             number_of_characters_in_password = number_of_characters_in_password + number_of_digits_in_password
             digits_in_password=choices(digits,k=number_of_digits_in_password)
             # print(number_of_digits_in_password)
@@ -42,49 +40,22 @@ def create_password():
             number_of_digits_in_password = 0
             # print(number_of_digits_in_password)
 
-        if inputs[5] in yes:
-            number_of_special_chrs_in_password = randint(1, inputs[1])
+        if special_chrs_exists in yes:
+            number_of_special_chrs_in_password = randint(1, password_len)
             number_of_characters_in_password = number_of_characters_in_password + number_of_special_chrs_in_password
             special_chrs_in_password=choices(special_chrs,k=number_of_special_chrs_in_password)
-            # print(number_of_special_chrs_in_password)
+
         else:
             number_of_special_chrs_in_password = 0
-            # print(number_of_small_letters_in_password)
         number_of_characters_in_password=number_of_capitalized_letters_in_password+number_of_small_letters_in_password+number_of_digits_in_password+number_of_special_chrs_in_password
-    print(number_of_capitalized_letters_in_password)
-    print(number_of_small_letters_in_password)
-    print(number_of_digits_in_password)
-    print(number_of_special_chrs_in_password)
-    print(captalized_letters_in_password)
-    print(small_letters_in_password)
-    print(digits_in_password)
-    print(special_chrs_in_password)
+
     generated_password=generated_password+captalized_letters_in_password
     generated_password=generated_password+small_letters_in_password
     generated_password=generated_password+digits_in_password
     generated_password=generated_password+special_chrs_in_password
-    print(generated_password)
     shuffle(generated_password)
-    print(generated_password)
     password_string=password_string.join(generated_password)
-    print(password_string)
     return password_string
-
-
-
-
-
-
-    #     characters_in_password = capitalized_letters_in_password + small_letters_in_password + digits_in_password + special_chrs_in_password
-    #     while not characters_in_password == inputs[1]:
-    #         capitalized_letters_in_password = randint(1, inputs[1])
-    #         small_letters_in_password = randint(1, inputs[1])
-    #         digits_in_password = randint(1, inputs[1])
-    #         special_chrs_in_password = randint(1, inputs[1])
-    #         characters_in_password = capitalized_letters_in_password + small_letters_in_password + digits_in_password + special_chrs_in_password
-    # print(characters_in_password)
-    # print(capitalized_letters_in_password)
-
 def validate_password(password_length):
     x = 0
     while x == 0:
@@ -99,8 +70,6 @@ def validate_password(password_length):
             # print(
             #     "You have to enter a valid number for the password length, it should be a whole positive number. Please try again")
     return password_length
-
-
 def validate_user_input(user_input, input_type):
     valid_yes_answer = ["Yes", "yes", "y", "Y"]
     valid_no_answer = ["No", "no", "n", "N"]
@@ -117,36 +86,36 @@ def validate_user_input(user_input, input_type):
         except ValueError as e:
             print(e)
     return user_input
-
-
-def ask_for_input():
-    file_name = input("What should be the name of the file where the passsword will be added?\n")
-    captalized_letters_exists = ""
-    small_letters_exists = ""
-    password_len = ""
-    digits_exists = ""
-    special_chrs_exists = ""
-    password_len = validate_password(password_len)
-    captalized_letters_exists = validate_user_input(captalized_letters_exists, "capitalized letters")
-    small_letters_exists = validate_user_input(small_letters_exists, "small letters")
-    digits_exists = validate_user_input(digits_exists, "digits")
-    special_chrs_exists = validate_user_input(special_chrs_exists, "special characters")
-    return [file_name, password_len, captalized_letters_exists, small_letters_exists, digits_exists,
-            special_chrs_exists]
-
-
-
-
-
-
 small_letters = [chr(x) for x in range(97, 123)]
 captalized_letters = [chr(x) for x in range(65, 91)]
 digits = [chr(x) for x in range(48, 58)]
-special_chrs = [chr(x) for x in range(32, 48)] + ([chr(x) for x in range(58, 65)]) + (
+special_chrs = [chr(x) for x in range(33, 48)] + ([chr(x) for x in range(58, 65)]) + (
 [chr(x) for x in range(91, 97)]) + ([chr(x) for x in range(123, 127)])
 yes = ["Yes", "yes", "y", "Y"]
 no = ["No", "no", "n", "N"]
-run=create_password()
-print(run)
+file_name = input("What should be the name of the file where the passsword will be added?\n")
+captalized_letters_exists = ""
+small_letters_exists = ""
+password_len = ""
+digits_exists = ""
+special_chrs_exists = ""
+password_len = validate_password(password_len)
+captalized_letters_exists = validate_user_input(captalized_letters_exists, "capitalized letters")
+small_letters_exists = validate_user_input(small_letters_exists, "small letters")
+digits_exists = validate_user_input(digits_exists, "digits")
+special_chrs_exists = validate_user_input(special_chrs_exists, "special characters")
+password=create_password()
+print(password)
+f_name=f"{file_name}.txt"
+if not path.exists(f_name):
+    with open(f_name, "w") as f:
+        if(len(password)>0):
+            f.write(f"{password}\n")
+else:
+    with open(f_name,"a") as f:
+        if (len(password) > 0):
+            f.write(f"{password}\n")
+
+
 
 
